7ca96fae7bd211aa45941b67459deeaaa2a6b10b
waxzdert <waxzdert162gmail.com>
2018-02-24 20:43:24 +0800

    add app

---
+++ Fermentation_box_app/Fermentation_box_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fermentation_box_app
+//
+//  Created by 吳東燁 on 2018/2/24.
+//  Copyright © 2018年 吳東燁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
